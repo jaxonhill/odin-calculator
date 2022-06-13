f4dd4adf8a54e930e7300eb4637dc8e4d2cbbeae
jaxonhill <jaxoncharleshill@gmail.com>
2022-06-12 21:09:19 -0700

    Add basic calc functions

---
+++ script.js
@@ -0,0 +1,24 @@
+// Create the addition function
+function add(a, b) {
+    return a + b;
+}
+
+// Create the subtraction function
+function subtract(a, b) {
+    return a - b;
+}
+
+// Create the multiply function
+function multiply(a, b) {
+    return a * b;
+}
+
+// Create the division function
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(85, 15));
+console.log(subtract(85, 15));
+console.log(multiply(85, 15));
+console.log(divide(85, 15));
